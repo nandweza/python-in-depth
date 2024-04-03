325f160910e4e0b5cbc8e9ab1c3c91af46337011
nandweza <allannandweza@gmail.com>
2024-04-02 19:54:57 -0600

    completed day 5

---
+++ Day_5/average_height.py
@@ -0,0 +1,44 @@
+"""
+You are going to write a program that calculates the average student height from a List of heights.
+
+e.g. student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+The average height can be calculated by adding all the heights together and dividing by the total number of heights.
+
+e.g.
+
+180 + 124 + 165 + 173 + 189 + 169 + 146 = 1146
+
+There are a total of 7 heights in student_heights
+
+1146 ÷ 7 = 163.71428571428572
+
+Average height rounded to the nearest whole number = 164
+
+Important You should not use the sum() or len() functions in your answer. You should try to replicate their functionality 
+using what you have learnt about for loops.
+"""
+
+# Input a Python list of student heights
+student_heights = input().split()
+
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+  
+# Write your code below this row 👇
+total_height = 0
+
+for height in student_heights:
+  total_height += height
+print(f"Total height: {total_height}")
+
+number_of_students = 0
+
+for student in student_heights:
+  number_of_students += 1
+print(f"Number of students: {number_of_students}")
+
+average_height = round(total_height / number_of_students)
+
+print(f"Average height: {average_height}")
