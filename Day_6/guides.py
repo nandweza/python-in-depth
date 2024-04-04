4faed7a1a9a303b6e5aebcc8fe41beb17d73fc9f
nandweza <allannandweza@gmail.com>
2024-04-04 05:36:00 -0600

    updates

---
+++ Day_6/guides.py
@@ -0,0 +1,17 @@
+#https://docs.python.org/3/library/functions.html
+#https://peps.python.org/pep-0008/
+
+"""
+#for loop
+for item in list_of_items:
+    #Do something to each item
+
+#for loop and range
+for number in range(a, b):
+    print(number)
+
+#while loop
+while something_is_true:
+    #Do something repeatedly
+
+"""
